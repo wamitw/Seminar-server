85cb2f8e787f7b78c51a95df700999c4c3cf7c64
Amit Abecasis <wamitw@gmail.com>
2019-12-28 11:00:53 +0200

    server [Refactoring]: Add prints

--- server.py
+++ server.py
@@ -6,13 +6,17 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Binding...")
     s.bind((HOST, PORT))
+    print("Listening...")
     s.listen()
     conn, addr = s.accept()
+    print("Got new socket!")
     with conn:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
+            print("Received data")
             if not data:
                 break
             conn.sendall(data)
