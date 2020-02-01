e15380c22f9e1745701436298f5b8a878954eb7b
Amit Abecasis <wamitw@gmail.com>
2020-02-01 13:45:01 -0800

    server [New Feature]: Add argument parser

--- server.py
+++ server.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
-import socket
+import socket, argparse
 
+#Default Values
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+parser = argparse.ArgumentParser(description='Server that accept python remote code execution and returns their result')
+parser.add_argument('--host', default=HOST, action='store', type=str, help='Host to listen upon. default is ' + HOST + '.')
+parser.add_argument('--port','-p', default=PORT, action='store', type=int, help='Port to listen upon')
+
+args = parser.parse_args()
+
+host = args.host
+port = args.port
+
+print("Starting server on host " + host + " on port " + str(port) + ".")
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Binding...")
-    s.bind((HOST, PORT))
+    s.bind((host, port))
     print("Listening...")
     s.listen()
     conn, addr = s.accept()
